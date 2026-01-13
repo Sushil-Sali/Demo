fdc806ca2c8d1ede82243dac6ac3d328268e0129
Sushil-Sali <sushilsali65@gmail.com>
2026-01-13 17:00:00 +0530

    add main server file

---
+++ app.js
@@ -0,0 +1,7 @@
+console.log("Hello, World!");
+console.log("This is a simple Node.js application.");
+console.log("Enjoy coding!");
+console.log("Hello, World!");
+console.log("This is a simple Node.js application.");
+console.log("Enjoy coding!");
+console.log("Hello, World!");
